d03110f9255a712eef412990e41594cdd23a032b
EmiyaKiritsugu3 <inamarjunior2@gmail.com>
2021-01-24 18:14:10 +0000

    Start

---
+++ main.c
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Sistema de Gestão Escolar                      ///
+///                Developed by  @flgorgonio - Jan, 2021                    ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menuSobre(void);
+
+int main(void) {
+    menuSobre();
+    return 0;
+
+}
+
+
+void menuSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto SIG-Finance                                    ///\n");
+    printf("///                Developed by  @joseinamar - Jan, 2021                    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                   = = = = = SIG-Finance = = = = =                       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  O controle de finanças domésticas é uma das atividades necessárias em  ///\n");
+    printf("///  praticamente todas as residências, uma vez que para conseguir equili-  ///\n");
+    printf("///  brar o orçamento doméstico e evitar o acúmulo de dívidas, se faz ne-   ///\n");
+    printf("///  cessário monitorar entradas e saídas de recursos. Tal atividade pode   ///\n");
+    printf("///  demandar tempo e esforço, principalmente se for realizado sem a ajuda  ///\n");
+    printf("///  de um software. O presente projeto tem como objetivo desenvolver um    ///\n");
+    printf("///  programa de computador que realize o acompanhamento do fluxo finan-    ///\n");
+    printf("///  ceiro em uma residência, evitando o descontrole de gastos.             ///\n"); 
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
